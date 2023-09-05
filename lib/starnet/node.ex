039c2dea9bf83745c0d1a9a0e04b62ea70396376
le in <mail.an.levin@gmail.com>
2023-09-05 02:12:31 +0200

    seperated startnet

---
+++ lib/starnet/node.ex
@@ -0,0 +1,34 @@
+defmodule Starnet.Node do
+  @moduledoc """
+    This module defines the single nodes in the star network.
+    A node holds state about the existing data on it.
+  """
+
+  use GenServer
+  require Logger
+
+  def start_link(params) do
+    GenServer.start_link(__MODULE__, params)
+  end
+
+  def init(params) do
+    Logger.debug("*** starting node ***")
+
+    {:ok,  
+      %{
+        ip: "64.9.78.21",
+        name: params.name,
+        flash: [],
+        c_wires: []
+      },
+      {:continue, :setup}
+    }
+  end
+
+  def handle_continue(:setup, state) do
+    Phoenix.PubSub.subscribe(Nets.PubSub, "star")
+    Phoenix.PubSub.subscribe(Nets.PubSub, "node")
+
+    {:noreply, state}
+  end
+end
